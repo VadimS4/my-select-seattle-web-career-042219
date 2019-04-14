71e7f885d897f335aebbd853804d4ccaa968dbf1
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-14 09:59:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,10 @@ def my_select(collection)
  i = 0
  collect = []
     while i < collection.length
-      if yield(collection[i])
-        collect << collection
-        i += 1
+        if yield(collection[i])
+          collect << collection
+        end
+      i += 1
     end
   collect
 end
